6ff610237d5328b17b812ccba2404831fd024052
po1yb1ank <b00t.b00t@yandex.ru>
2021-06-01 23:52:48 +0300

    main func

--- cmd/balance.go
+++ cmd/balance.go
@@ -1,1 +1,20 @@
-package cmd
+package main
+
+import (
+	"github.com/auctionee/balance/pkg/logger"
+	"github.com/auctionee/balance/pkg/server"
+	"sync"
+)
+
+const DEFAULT_PORT = 8080
+
+func main() {
+	wg := sync.WaitGroup{}
+	s := server.NewAuthServer(DEFAULT_PORT)
+	wg.Add(1)
+	go func() {
+		logger.GetLogger(s.Ctx).Println("Listening on:", DEFAULT_PORT)
+		s.Start()
+	}()
+	wg.Wait()
+}
